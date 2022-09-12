8f839895b6f23329f79fc33583bb1b8b0e53d1f2
Damyn Chipman <DamynChipman@u.boisestate.edu>
2022-09-12 10:45:01 -0600

    Initial commit; creating app and infrastructure.

---
+++ src/HydroForestApp.hpp
@@ -0,0 +1,34 @@
+#ifndef HYDRO_FOREST_APP_HPP_
+#define HYDRO_FOREST_APP_HPP_
+
+#include <p4est.h>
+#include <petsc.h>
+#include "GenericSingleton.hpp"
+
+namespace hf {
+
+class HydroForestApp : public GenericSingleton<HydroForestApp> {
+
+private:
+
+    int* argc_;
+    char*** argv_;
+
+public:
+    
+    HydroForestApp(int* argc, cahr*** argv) : argc_(argc), argv_(argv) {
+        std::cout << "[HydroForest] Welcome to HydroForest!" << std::endl;
+        std::cout << "[HydroForest] Initializing MPI and PETSc..." << std::endl;
+        MPI_Init(argc_, argv_);
+        PetscInitialize(argc_, argv_, NULL, NULL);
+    }
+
+    int* getArgc() const { return argc_; }
+    char*** getArgv() const { return argv_; }
+
+}
+
+
+} // NAMESPACE: hf
+
+#endif // HYDRO_FOREST_APP_HPP_
